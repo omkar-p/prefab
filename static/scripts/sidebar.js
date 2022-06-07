4ef167664488afc8b9ed1f82e40a46081bad59c6
Omkar Patil <omkar.patil1206@gmail.com>
2022-06-07 22:26:24 +0530

    Added model carosul row and js for the same. Removed auto changing nature of top header's background.

--- static/scripts/sidebar.js
+++ static/scripts/sidebar.js
@@ -7,7 +7,6 @@ $(function(){
     var current = location.pathname;
     $('#left-menu-bar a').each(function(){
         var $this = $(this);
-        console.log($this, $this.attr('href'));
         // if the current path is like this link, make it active
         if(current.indexOf($this.attr('href')) !== -1){
             $this.addClass('active');
